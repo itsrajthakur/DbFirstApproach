6975bb792c4415258cc49119ff88960a76bd8032
rajsinghthakure <rajthakur8827142011@gmail.com>
2024-10-03 07:55:51 +0530

    first commit

---
+++ DbFirstApproach/Models/ApplicationDbContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbFirstApproach.Models;
+
+public partial class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){ }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__products__3214EC07A075FD4A");
+
+            entity.ToTable("products");
+
+            entity.Property(e => e.Brand)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07F256E6A2");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D1053437A48749").IsUnique();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.UserName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DbFirstApproach/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbFirstApproach.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? ProductName { get; set; }
+
+    public string? Brand { get; set; }
+
+    public int? Price { get; set; }
+}
